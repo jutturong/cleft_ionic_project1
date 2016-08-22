e5f5ace0775573b7ea0ef4fa9f9e2406186e9fd4
jutturong <jutturongcharoenrit@gmail.com>
2016-08-22 10:51:39 +0700

    1 22-8-59

--- project1/app/pages/home/home.ts
+++ project1/app/pages/home/home.ts
@@ -1,5 +1,5 @@
-import {Component  } from '@angular/core';
-import {NavController,MenuController,ViewController,ModalController,NavParams , Platform } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {NavController,MenuController,ViewController } from 'ionic-angular';
 
 import { PageContentPage1 }  from '../page-content/page-content';
 //import { TabsPage } from '../tabs/tabs';
@@ -12,11 +12,10 @@ export class HomePage {
      public items:any;
      public viewCtrl: ViewController;
 
-
-  constructor(private navCtrl: NavController,private modalController : ModalController) {
+  constructor(private navCtrl: NavController) {
 
     this.items=[
-        { title:'ภาวะปากแหว่งเพานโหว่',description:'1',icon:'md-contacts',num:0,component: PageContentPage1  },
+        { title:'ภาวะปากแหว่งเพานโหว่',description:'1',icon:'md-contacts',num:0,component: PageContentPage1 , image: 'image/avatar-frodo.jpg', },
         { title:'สาเหตุ',description:'2',icon:'ios-git-compare-outline',num:1 , },
         { title:'โอกาสจะเกิดซ้ำ',description:'3',icon:'ios-git-network-outline',num:2 , },
         { title:'ปัญหาที่พบและการแก้ไข',description:'4',icon:'ios-image',num:3 , },
@@ -27,80 +26,21 @@ export class HomePage {
   }
 
 
-
-
   openPage(items) {
+    // close the menu when clicking a link from the menu
+  //  this.menu.close();
+    // navigate to the new page if it is not the current page
     this.navCtrl.setRoot(items.component);
-    // let modal = this.modalController.create(items.component);
-    // modal.present();
   }
 
-
-  openModal(characterNum) {
-  let modal = this.modalController.create(ModalsContentPage, characterNum);
-  modal.present();
-  }
-
-
-
-}
+/*
+  goBack() {
+     //this.navCtrl.pop();
+      this.navCtrl.push(TabsPage);
+   }
+*/
 
 
 
 
-@Component({
-templateUrl: 'build/pages/page-content/page-content2.html'
-})
-
-class ModalsContentPage {
-character;
-constructor(
-public platform: Platform,
-public params: NavParams,
-public viewCtrl: ViewController
-) {
-var characters = [
-{
-name: 'Gollum',
-quote: 'Sneaky little hobbitses!',
-image: 'img/avatar-gollum.jpg',
-items: [
-{ title: 'Race', note: 'Hobbit' },
-{ title: 'Culture', note: 'River Folk' },
-{ title: 'Alter Ego', note: 'Smeagol' }
-]
-},
-{
-name: 'Frodo',
-quote: 'Go back, Sam! I\'m going to Mordor alone!',
-image: 'img/avatar-frodo.jpg',
-items: [
-{ title: 'Race', note: 'Hobbit' },
-{ title: 'Culture', note: 'Shire Folk' },
-{ title: 'Weapon', note: 'Sting' }
-]
-},
-{
-name: 'Samwise Gamgee',
-quote: 'What we need is a few good taters.',
-image: 'img/avatar-samwise.jpg',
-items: [
-{ title: 'Race', note: 'Hobbit' },
-{ title: 'Culture', note: 'Shire Folk' },
-{ title: 'Nickname', note: 'Sam' }
-]
-}
-];
-this.character = characters[this.params.get('charNum')];
-}
-
-
-
-
-
-
-
-dismiss() {
-this.viewCtrl.dismiss();
-}
 }
